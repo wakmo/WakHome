9daa024ecfcba8e8f6ff0386632ea2921bf5ae87
wm-aconite <wakkir.muzammil@aconite.net>
2014-07-08 13:41:58 +0100

    added jUnit test for multithreaded

---
+++ Workspaces/intelij/home/mytest/mymaven/mymaven-core/src/main/java/wak/test/sort/FindTopValues1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wak.test.sort;
+
+/**
+ *
+ * @author wakkir.muzammil
+ */
+public interface FindTopValues1
+{
+    int findMaxValue(int[] anyOldOrderValues);
+    int[] findTopNValues(int[] anyOldOrderValues, int n);
+
+}
