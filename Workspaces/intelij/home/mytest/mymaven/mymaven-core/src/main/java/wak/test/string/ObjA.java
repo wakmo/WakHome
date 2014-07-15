870861e1d1517a9edaa1404aeaed83d8839a405f
wm-aconite <wakkir.muzammil@aconite.net>
2014-07-15 10:49:44 +0100

    Hashcode added

---
+++ Workspaces/intelij/home/mytest/mymaven/mymaven-core/src/main/java/wak/test/string/ObjA.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wak.test.string;
+
+/**
+ *
+ * @author wakkir.muzammil
+ */
+public class ObjA
+{
+
+    private int id;
+    private String name;
+    
+
+    public ObjA(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+    
+    @Override
+    public int hashCode()
+    {
+        int hash = 7;
+        hash = 37 * hash + this.id;
+        hash = 37 * hash + (this.name != null ? this.name.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        final ObjA other = (ObjA) obj;
+        if (this.id != other.id)
+        {
+            return false;
+        }
+        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name))
+        {
+            return false;
+        }
+        return true;
+    }
+    
+    
+
+    @Override
+    public String toString()
+    {
+        return "ObjA{" + "id=" + id + ", name=" + name + '}';
+    }
+       
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
---
+++ Workspaces/intelij/home/mytest/mymaven/mymaven-core/src/main/java/wak/test/string/ObjB.java
@@ -0,0 +1,86 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wak.test.string;
+
+/**
+ *
+ * @author wakkir.muzammil
+ */
+public class ObjB
+{
+
+    private int id;
+    private String name;
+    
+
+    public ObjB(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+    
+    @Override
+    public int hashCode()
+    {
+        int hash = 3;
+        hash = 67 * hash + this.id;
+        hash = 67 * hash + (this.name != null ? this.name.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        final ObjB other = (ObjB) obj;
+        if (this.id != other.id)
+        {
+            return false;
+        }
+        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name))
+        {
+            return false;
+        }
+        return true;
+    }
+
+   
+    
+    
+
+    @Override
+    public String toString()
+    {
+        return "ObjB{" + "id=" + id + ", name=" + name + '}';
+    }
+       
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
