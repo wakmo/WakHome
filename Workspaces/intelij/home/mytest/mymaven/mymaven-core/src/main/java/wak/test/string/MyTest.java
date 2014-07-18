edda2d9029976b0e716b7c3877df4da33a27c672
wm-aconite <wakkir.muzammil@aconite.net>
2014-07-18 15:23:43 +0100

    added my test

---
+++ Workspaces/intelij/home/mytest/mymaven/mymaven-core/src/main/java/wak/test/string/MyTest.java
@@ -0,0 +1,19 @@
+
+package wak.test.string;
+
+/**
+ *
+ * @author wakkir.muzammil
+ */
+public class MyTest
+{
+     
+    public static void main(String[] args)
+    {
+        int x=0;
+        int y=x+1;
+        System.out.print(x);
+    
+    }
+    
+}
